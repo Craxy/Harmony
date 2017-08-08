8ceb2ad5caf6b73c3bb04b8ca2f0cfe83d66fdb7
Craxy <Craxyss@gmail.com>
2017-08-08 20:15:23 +0200

    Remove PatchFunctions, now included in Patch

--- Harmony/Patch.cs
+++ Harmony/Patch.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using Harmony.ILCopying;
 
 namespace Harmony
 {
@@ -8,10 +11,10 @@ namespace Harmony
   {
     private MethodBase _original;
     private MethodInfo _postfix;
-    private PatchFunctions.PatchHandle _patchHandle;
+    private PatchHandle _patchHandle;
     public bool IsPatched => _patchHandle != null && !_patchHandle.IsDisposed;
 
-    private Patch(MethodBase original, MethodInfo postfix, PatchFunctions.PatchHandle patchHandle)
+    private Patch(MethodBase original, MethodInfo postfix, PatchHandle patchHandle)
     {
       _original = original;
       _postfix = postfix;
@@ -21,14 +24,14 @@ namespace Harmony
     /// <returns>Must be kept alive</returns>
     public static Patch Apply(MethodInfo original, MethodInfo postfix)
     {
-      return new Patch(original, postfix, PatchFunctions.Update(original, postfix));
+      return new Patch(original, postfix, Apply((MethodBase) original, postfix));
     }
 
     public void Restore()
     {
       if (IsPatched)
       {
-        PatchFunctions.Restore(_original, _patchHandle);
+        Restore(_original, _patchHandle);
       }
       Debug.Assert(!IsPatched);
       
@@ -44,5 +47,68 @@ namespace Harmony
     }
 
     public static bool DEBUG = false;
+    
+    private class PatchHandle : IDisposable
+    {
+      public DynamicMethod PatchedMethod { get; private set; }
+      public byte[] OverwrittenCode { get; private set; }
+
+      public PatchHandle(DynamicMethod patchedMethod, byte[] overwrittenCode)
+      {
+        PatchedMethod = patchedMethod ?? throw new ArgumentNullException(nameof(patchedMethod));
+        OverwrittenCode = overwrittenCode ?? throw new ArgumentNullException(nameof(overwrittenCode));
+      }
+
+      public void Destroy() 
+        => Dispose();
+      public void Dispose()
+      {
+        PatchedMethod = null;
+        OverwrittenCode = null;
+      }
+
+      public bool IsDisposed => PatchedMethod == null;
+    }
+    private static PatchHandle Apply(MethodBase original, MethodInfo postfix)
+    {
+      if (postfix == null)
+      {
+        throw new ArgumentNullException(nameof(postfix));
+      }
+			
+      var originalCodeStart = Memory.GetMethodStart(original);
+
+      var replacement = MethodPatcher.CreatePatchedMethod(original, postfix);
+      if (replacement == null)
+      {
+        throw new MissingMethodException($"Cannot create dynamic replacement for {original}");
+      }
+      var patchCodeStart = Memory.GetMethodStart(replacement);
+			
+      // This part effectively corrupts the original compiled method, so we should prepare to restore the overwritten part later
+      // (It doesn't look like it breaks something, but... better safe than sorry?)
+      var oldBytes = new byte[IntPtr.Size == sizeof(long) ? 12 : 6];
+      Marshal.Copy((IntPtr)originalCodeStart, oldBytes, 0, oldBytes.Length);
+
+      Memory.WriteJump(originalCodeStart, patchCodeStart);
+
+      return new PatchHandle(patchedMethod: replacement, overwrittenCode: oldBytes);
+    }
+    private static void Restore(MethodBase original, PatchHandle patchHandle)
+    {
+      if (patchHandle == null)
+      {
+        throw new ArgumentNullException(nameof(patchHandle));
+      }
+      if (patchHandle.IsDisposed)
+      {
+        throw new InvalidOperationException($"{nameof(patchHandle)} is disposed!");
+      }
+			
+      var originalCodeStart = Memory.GetMethodStart(original);
+      Memory.WriteBytes(originalCodeStart, patchHandle.OverwrittenCode);
+			
+      patchHandle.Dispose();
+    }
   }
 }
